1181e54ec3f8829e7fd978a715b1632d7407af5a
tpreston96 <tpreston96@gmail.com>
2020-12-16 23:26:10 -0600

    manga routes

--- routes/manga.js
+++ routes/manga.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const mangaCtrl = require('../controllers/manga')
 
 router.get('/new', isLoggedIn, mangaCtrl.new)
+router.get('/', isLoggedIn, mangaCtrl.index)
+router.post('/search', isLoggedIn, mangaCtrl.search)
+router.get('/:slug', isLoggedIn, mangaCtrl.show)
 
 
 function isLoggedIn(req, res, next) {
